095d1de5e0047255d13bfdf1aaa20e69800f7588
Shivam-RN <shivamrana1809@gmail.com>
2025-08-01 21:01:31 +0530

    Build all pages

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import { useAuth, useUser } from "@clerk/clerk-react";
+import React, { useEffect, useState } from "react";
+import { Heart } from "lucide-react";
+import {dummyPublishedCreationData} from '../assets/assets'
+
 
 const Community = () => {
-  return (
-    <div>
-      coumunotu
-    </div>
-  )
-}
+  const [creations, setCreations] = useState([]);
+  const { user } = useUser();
+  const [loading, setLoading] = useState(false);
+  
 
-export default Community
+  const fetchCreations = async () => {
+    setCreations(dummyPublishedCreationData)
+  };
+
+
+
+  useEffect(() => {
+    if (user) {
+      fetchCreations();
+    }
+  }, [user]);
+
+  return !loading ? (
+    <div className="flex-1 h-full flex flex-col gap-4 p-6">
+      Creations
+      <div className="bg-white h-full w-full rounded-xl overflow-y-scroll flex flex-wrap gap-4">
+        {creations.map((creation, index) => (
+          <div
+            key={index}
+            className="relative group inline-block w-full sm:w-[48%] lg:w-[30%] pl-3 pt-3"
+          >
+            <img
+              src={creation.content}
+              className="w-full h-full object-cover rounded-lg"
+            />
+
+            <div className="absolute bottom-0 top-0 right-0 left-3 flex gap-2 items-end justify-end group-hover:justify-between p-3 group-hover:bg-gradient-to-b from-transparent to-black/80 text-white rounded-lg">
+              <p className="text-sm hidden group-hover:block">
+                {creation.prompt}
+              </p>
+              <div className="flex gap-1 items-center">
+                <p>
+                  {Array.isArray(creation.likes) ? creation.likes.length : 0}
+                </p>
+                <Heart
+                  className={`min-w-5 h-5 hover:scale-110 cursor-pointer ${
+                   
+                    creation.likes.includes(user.id)
+                      ? "fill-red-500 text-red-600"
+                      : "text-white"
+                  }`}
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div className="flex justify-center items-center h-full">
+      <span className="w-10 h-10 my-1 rounded-full border-3 border-primary border-t-transparent animate-spin"></span>
+    </div>
+  );
+};
+
+export default Community;
