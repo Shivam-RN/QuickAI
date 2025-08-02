1aa7760bf80e7ec38c8618c67dba415a783de4e8
Shivam-RN <shivamrana1809@gmail.com>
2025-08-02 13:07:24 +0530

    Initialize backend and create middleware

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import "dotenv/config"
+import { clerkMiddleware, requireAuth } from '@clerk/express'
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+app.use(clerkMiddleware()) 
+
+app.get('/', (req,res) => {
+  res.send('Hello Wld!')
+})
+app.use(requireAuth())
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
